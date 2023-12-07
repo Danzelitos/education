2696de45b722684a0de0e9ff0a443cff1107b0ec
Danzelitos <danzeliti@gmail.com>
2023-12-07 10:45:19 +0300

    07.12

--- week16 calcCars/calcar.js
+++ week16 calcCars/calcar.js
@@ -18,7 +18,6 @@ output.textContent = mid + " литра";
 rangeInput.addEventListener('input', function() {
   const value = +rangeInput.value ; // Преобразуем значение диапазона в соответствующее число
   output.textContent = value + ' литра'; // Выводим значение в элемент output
-  // console.log(value); // Выводим значение в консоль
 	if (value === 3.5) {
 		console.log('maximum');
 	} if (value === 1.1) {
@@ -27,61 +26,11 @@ rangeInput.addEventListener('input', function() {
 });
 
 
-finalBtn.addEventListener('click', function(){
-	fuelOptions.forEach(function(radio){
-		if(radio.checked){
-			if (radio.value === 'gasoline'){
-				console.log('Бензин');
-				fullPrice = 0;
-				fullPrice += 100;
-			} else if (radio.value === 'dizel'){
-				console.log('Дизель');
-				fullPrice = 0;
-				fullPrice += 200;
-			} else if (radio.value === 'gas'){
-				console.log('Газ');
-				fullPrice = 0;
-				fullPrice += 300;
-			} else if (radio.value === 'electricity'){
-				console.log('Электричество');
-				fullPrice = 0;
-				fullPrice += 400;
-			};
-			console.log(fullPrice);
-		};
-	});
-	vehicleStatus.forEach(function(radio){
-		if(radio.checked){
-			if (radio.value === 'newCar'){
-				console.log('Новый');
-				fullPrice = 0;
-				fullPrice += 100;
-			} else if (radio.value === 'oldCar'){
-				console.log('Подержанный');
-				fullPrice = 0;
-				fullPrice += 200;
-			};
-		};
-	});
-	carOwners.forEach(function(radio){
-		if(radio.checked){
-			if (radio.value === '1-2 owners'){
-				console.log('1-2 владельца');
-				fullPrice = 0;
-				fullPrice += 100;
-			} else if (radio.value === '3 more'){
-				console.log('более 3-х владельцев');
-				fullPrice = 0;
-				fullPrice += 200;
-			};
-		};
-	})
-});
-
 function selectModel(){
 	if (selectMarks.value === 'Reno'){
 		selectModels.disabled = false;
 		selectModels.innerHTML = "";
+		fullPrice += 1000000;
 		let renoCars = ['Kaptur', 'Duster', 'Logan', 'Sandero'];
 		let renoPrice = [1000000, 1100000, 1200000, 1300000];
 		for (let i = 0 ; i < renoCars.length; i++){
@@ -92,6 +41,7 @@ function selectModel(){
 	} else if (selectMarks.value === 'Opel'){
 		selectModels.disabled = false;
 		selectModels.innerHTML = "";
+		fullPrice += 1100000;
 		let opelCars = ['Astra', 'Corsa', 'Mokka', 'Antara'];
 		let opelPrice = [1000000, 1100000, 1200000, 1300000];
 		for (let i = 0 ; i < opelCars.length; i++){
@@ -102,6 +52,7 @@ function selectModel(){
 	} else if (selectMarks.value === 'Mazda'){
 		selectModels.disabled = false;
 		selectModels.innerHTML = "";
+		fullPrice += 1200000;
 		let mazdaCars = ['MX-5', 'CX-9', 'CX-7', 'Mazda 3'];
 		let mazdaPrice = [1000000, 1100000, 1200000, 1300000];
 		for (let i = 0 ; i < mazdaCars.length; i++){
@@ -112,6 +63,7 @@ function selectModel(){
 	} else if (selectMarks.value === 'Jaguar'){
 		selectModels.disabled = false;
 		selectModels.innerHTML = "";
+		fullPrice += 1300000;
 		let JaguarCars = ['FX', 'XFR', 'X-type', 'XKR'];
 		let JaguarPrice = [1000000, 1100000, 1200000, 1300000];
 		for (let i = 0 ; i < JaguarCars.length; i++){
@@ -129,6 +81,57 @@ selectMarks.addEventListener('change', function(event){
 
 formCar.addEventListener('submit', function(event){
 	event.preventDefault();
+
+	fuelOptions.forEach(function(radio){
+		if(radio.checked){
+			if (radio.value === 'gasoline'){
+				console.log('Бензин');
+				// fullPrice = 0;
+				fullPrice += 450000;
+			} else if (radio.value === 'dizel'){
+				console.log('Дизель');
+				// fullPrice = 0;
+				fullPrice += 400000;
+			} else if (radio.value === 'gas'){
+				console.log('Газ');
+				// fullPrice = 0;
+				fullPrice += 350000;
+			} else if (radio.value === 'electricity'){
+				console.log('Электричество');
+				// fullPrice = 0;
+				fullPrice += 500000;
+			};
+			// console.log(fullPrice);
+		};
+	});
+	vehicleStatus.forEach(function(radio){
+		if(radio.checked){
+			if (radio.value === 'newCar'){
+				console.log('Новый');
+				// fullPrice = 0;
+				fullPrice += 1000000;
+			} else if (radio.value === 'oldCar'){
+				console.log('Подержанный');
+				// fullPrice = 0;
+				fullPrice += 750000;
+			};
+		};
+	});
+	const ownersError = document.querySelector('.ownersError');
+	carOwners.forEach(function(radio){
+		if(radio.checked){
+			if (radio.value === '1-2 owners'){
+				console.log('1-2 владельца');
+				// fullPrice = 0;
+				fullPrice += 300000;
+			} else if (radio.value === '3 more'){
+				console.log('более 3-х владельцев');
+				// fullPrice = 0;
+				fullPrice += 200000;
+			};
+		};
+	});
+	totalPrice.textContent = `Цена автомобиля по вашим параметрам составит ${fullPrice}`
 });
 
 
--- week16 registration/registTry.js
+++ week16 registration/registTry.js
@@ -1,25 +1,187 @@
 const form = document.forms[0];
 const allInputs = document.querySelectorAll('.input');
-const checkBox = document.querySelector('.js-input-checkBox');
 const errorEnd = document.querySelector('.error-message');
 
 
+// // Поле Имя:
+// const inputName = form.elements.firstName;
+// inputName.addEventListener('input', function(event){
+// 	const nameError = document.querySelector('.nameError');
+// 	console.log(event.target.validity);
+// 	if (!inputName.validity.valid){
+// 		inputName.classList.add("error");
+// 		nameError.textContent = inputName.validationMessage;
+// 		nameError.style.color = 'red';
+// 		isFormValid = false;
+// 	} else{
+// 		inputName.classList.remove("error");
+// 		nameError.textContent = '';
+// 	}
+// });
+
+
+// // Поле Email:
+// const emailInput = document.querySelector('.email');
+// emailInput.addEventListener('input', function(event){
+// 	const emailError = document.querySelector('.emailError')
+// 	console.log(event.target.validity);
+// 	if(!emailInput.validity.valid){
+// 		emailInput.classList.add('error');
+// 		emailError.textContent = emailInput.validationMessage;
+// 		emailError.style.color = 'red';
+// 	} else {
+// 		emailInput.classList.remove('error');
+// 		emailError.textContent = '';
+// 	}
+// });
+
+
+// Поле возраста:
+// const age = document.querySelector('.age');
+// const ageError = document.querySelector(".ageError");
+// age.addEventListener('input', function(event) {
+//   const value = parseInt(event.target.value, 10);
+//   if (value <= 1 || value >= 100) {
+//     age.setCustomValidity('Введите число больше 1 и меньше 100');
+// 		age.classList.add('error');
+//     ageError.textContent = 'Введите число больше 1 и меньше 100';
+// 		ageError.style.color = 'red';
+//   } else {
+//     age.setCustomValidity('');
+// 		age.classList.remove('error');
+//     ageError.textContent = '';
+//   }
+// });
+// // Выбор пола:
+// const radioButtons = document.querySelectorAll('.js-input-radio');
+// function checkRadios(){
+// 	let checked = false;
+// 	for (let radio of radioButtons){
+// 		if (radio.checked){
+// 			checked = true;
+// 			console.log(radio.value);
+// 			break;
+// 	}
+// }
+// const sexError = document.querySelector('.sexError')
+// 		if (!checked){
+// 			sexError.textContent = 'Выберите один из вариантов';
+// 			sexError.style.color = 'red';
+// 		} else {
+// 			sexError.textContent = ''
+// 		}
+// };
+// // Поле пароля:
+// const passwordInput = document.querySelector('.js-input-pass');
+// const passError = document.querySelector('.passError');
+
+// passwordInput.addEventListener('input', function(event){
+// 	const validity = event.target.validity;
+// 	if(!validity.patternMismatch){
+// 		const pass = event.target.value;
+// 		console.log(pass);
+// 		const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,20}$/;
+// 	if (!pattern.test(pass)){
+// 		passError.textContent = 'Пароль должен содержать по крайней мере одну заглавную букву, одну цифру и один специальный символ. Длина пароля должна быть от 8 до 20 символов.';
+// 		passError.style.color = 'red'
+// 		passwordInput.classList.add('error');
+// 	} else {
+// 		passwordInput.classList.remove('error');
+// 		passError.textContent = '';
+// 		passwordInput.classList.remove('error');
+// 		}
+// 	}
+// });
+// // Поле повтора пароля:
+// const repasswordInput = document.querySelector('.js-input-rePass');
+// const repassError = document.querySelector('.repassError');
+
+// repasswordInput.addEventListener('input', function(event){
+// 	const validity = event.target.validity;
+// 	if(!validity.patternMismatch){
+// 		const pass = event.target.value;
+// 		console.log(pass);
+// 		const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,20}$/;
+// 	if (!pattern.test(pass)){
+// 		repassError.textContent = 'Пароль должен содержать по крайней мере одну заглавную букву, одну цифру и один специальный символ. Длина пароля должна быть от 8 до 20 символов.';
+// 		repassError.style.color = 'red'
+// 		repasswordInput.classList.add('error');
+// 	} else {
+// 		repasswordInput.classList.remove('error');
+// 		repassError.textContent = '';
+// 		repasswordInput.classList.remove('error');
+// 		}
+// 	}
+// });
+// // Соответствие паролей друг другу
+// function checkPasswords(){
+// const password = passwordInput.value;
+// const repassword = repasswordInput.value;
+
+//   if (password != repassword){
+// 			errorEnd.textContent = 'Пароли не равны';
+//       repasswordInput.classList.add('error');
+//       passwordInput.classList.add('error');
+//       console.log(password, repassword);
+//     } else{
+//       repasswordInput.classList.remove('error');
+//       passwordInput.classList.remove('error');
+// 			errorEnd.textContent = '';
+//       console.log(password, repassword);
+//     }
+// 	};
+// 	// Проверка чекбокса
+// const labelBox = document.querySelector('.labelBox');
+// function ischeckBox(){
+// 	  if (checkBox.checked){
+// 		labelBox.style.color = 'black';
+//   } else {
+//     console.log('checkbox is not checked');
+//     labelBox.style.color = 'red';
+//   }
+// };
+
+form.addEventListener('submit', function(evt){
+	evt.preventDefault();
+	
+	let isFormValid = true;
+
 // Поле Имя:
+// inputName.addEventListener('input', function(event){
+// 	console.log(event.target.validity);
+// 	if (!inputName.validity.valid){
+// 		inputName.classList.add("error");
+// 		if (!inputName.value) {
+//       nameError.textContent = "Это обязательное поле";
+// 			isFormValid = false; 
+//     } else {
+//       nameError.textContent = inputName.validationMessage;
+//     };
+// 		nameError.textContent = inputName.validationMessage;
+// 		nameError.style.color = 'red';
+// 		console.log(inputName.checkValidity());
+// 		isFormValid = false;
+// 	} else{
+// 		inputName.classList.remove("error");
+// 		nameError.textContent = '';
+// 	}
+// });
+const nameError = document.querySelector('.nameError');
 const inputName = form.elements.firstName;
-inputName.addEventListener('input', function(event){
-	const nameError = document.querySelector('.nameError');
-	console.log(event.target.validity);
-	if (!inputName.validity.valid){
-		// if (!inputName.validity.rangeOverflow){
-		// 	if (!inputName.validity.rangeUnderflow){
-		inputName.classList.add("error");
+if (!inputName.validity.valid) {
+	inputName.classList.add("error");
+	if (!inputName.value) {
+		nameError.textContent = "Это обязательное поле";
+	} else {
 		nameError.textContent = inputName.validationMessage;
-		nameError.style.color = 'red';
-	} else{
-		inputName.classList.remove("error");
-		nameError.textContent = '';
 	}
-});
+	nameError.style.color = 'red';
+	isFormValid = false; // Устанавливаем флаг в false, если поле не прошло проверку
+} else {
+	inputName.classList.remove("error");
+	nameError.textContent = '';
+}
+
 // Поле Email:
 const emailInput = document.querySelector('.email');
 emailInput.addEventListener('input', function(event){
@@ -29,11 +191,13 @@ emailInput.addEventListener('input', function(event){
 		emailInput.classList.add('error');
 		emailError.textContent = emailInput.validationMessage;
 		emailError.style.color = 'red';
+		isFormValid = false;
 	} else {
 		emailInput.classList.remove('error');
 		emailError.textContent = '';
 	}
-})
+});
+
 // Поле возраста:
 const age = document.querySelector('.age');
 const ageError = document.querySelector(".ageError");
@@ -44,21 +208,22 @@ age.addEventListener('input', function(event) {
 		age.classList.add('error');
     ageError.textContent = 'Введите число больше 1 и меньше 100';
 		ageError.style.color = 'red';
+		isFormValid = false;
   } else {
     age.setCustomValidity('');
 		age.classList.remove('error');
     ageError.textContent = '';
   }
 });
+
 // Выбор пола:
 const radioButtons = document.querySelectorAll('.js-input-radio');
-// console.log(radioButtons);
 function checkRadios(){
 	let checked = false;
 	for (let radio of radioButtons){
 		if (radio.checked){
 			checked = true;
-			console.log(radio.value);
+			// console.log(radio.value);
 			break;
 	}
 }
@@ -66,10 +231,10 @@ const sexError = document.querySelector('.sexError')
 		if (!checked){
 			sexError.textContent = 'Выберите один из вариантов';
 			sexError.style.color = 'red';
+			isFormValid = false;
 		} else {
 			sexError.textContent = ''
 		}
-	
 };
 // Поле пароля:
 const passwordInput = document.querySelector('.js-input-pass');
@@ -85,10 +250,10 @@ passwordInput.addEventListener('input', function(event){
 		passError.textContent = 'Пароль должен содержать по крайней мере одну заглавную букву, одну цифру и один специальный символ. Длина пароля должна быть от 8 до 20 символов.';
 		passError.style.color = 'red'
 		passwordInput.classList.add('error');
+		isFormValid = false;
 	} else {
 		passwordInput.classList.remove('error');
 		passError.textContent = '';
-		passwordInput.classList.remove('error');
 		}
 	}
 });
@@ -106,31 +271,35 @@ repasswordInput.addEventListener('input', function(event){
 		repassError.textContent = 'Пароль должен содержать по крайней мере одну заглавную букву, одну цифру и один специальный символ. Длина пароля должна быть от 8 до 20 символов.';
 		repassError.style.color = 'red'
 		repasswordInput.classList.add('error');
+		isFormValid = false;
 	} else {
 		repasswordInput.classList.remove('error');
 		repassError.textContent = '';
-		repasswordInput.classList.remove('error');
 		}
 	}
 });
+
 // Соответствие паролей друг другу
 function checkPasswords(){
-const password = passwordInput.value;
-const repassword = repasswordInput.value;
+	const password = passwordInput.value;
+	const repassword = repasswordInput.value;
+	
+		if (password != repassword){
+				errorEnd.textContent = 'Пароли не равны';
+				repasswordInput.classList.add('error');
+				passwordInput.classList.add('error');
+				console.log(password, repassword);
+				isFormValid = false;
+			} else{
+				repasswordInput.classList.remove('error');
+				passwordInput.classList.remove('error');
+				errorEnd.textContent = '';
+				console.log(password, repassword);
+			}
+		};
 
-  if (password != repassword){
-			errorEnd.textContent = 'Пароли не равны';
-      repasswordInput.classList.add('error');
-      passwordInput.classList.add('error');
-      console.log(password, repassword);
-    } else{
-      repasswordInput.classList.remove('error');
-      passwordInput.classList.remove('error');
-			errorEnd.textContent = '';
-      console.log(password, repassword);
-    }
-
-	};
+// Проверка чекбокса
+const checkBox = document.querySelector('.js-input-checkBox');
 const labelBox = document.querySelector('.labelBox');
 function ischeckBox(){
 	  if (checkBox.checked){
@@ -138,30 +307,18 @@ function ischeckBox(){
   } else {
     console.log('checkbox is not checked');
     labelBox.style.color = 'red';
+		isFormValid = false;
   }
 };
 
-form.addEventListener('submit', function(evt){
-	evt.preventDefault();
 	checkRadios();
 	checkPasswords();
 	ischeckBox();
+
+	if (isFormValid) {
+    // Здесь можно добавить действия, которые выполняются при успешной валидации и отправке формы
+    form.submit(); // Форма будет отправлена, так как все поля прошли валидацию
+  } else {
+    errorEnd.textContent = "Форма содержит ошибки. Проверьте заполнение всех полей." + validationMessage; // Отображение сообщения о наличии ошибок в форме
+  }
 });
-
-
-
-
-
-
-
-
-
-// passwordInput.classList.add('error');
-// // passwordInput.setCustomValidity('Минимум 8 символов, 1 заглавная буква, 1 символ и 1 цифру')
-// passError.textContent = passwordInput.validationMessage;
-// passError.style.color = 'red'
-// } else {
-// // passwordInput.setCustomValidity('');
-// passwordInput.classList.remove('error');
-// passError.textContent = '';
-// }
